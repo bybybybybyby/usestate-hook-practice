b72184e48379df68b973f2edee0e5e87d522d927
Brian <bybyby@gmail.com>
2021-03-18 20:48:26 -0700

    Make button update time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function App() {
+  const [time, updateTime] = React.useState(new Date().toLocaleTimeString());
+
+  function getTime() {
+    updateTime(new Date().toLocaleTimeString());
+    console.log(time);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
 //Challenge:
 //1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
+// let time = new Date().toLocaleTimeString();
+// console.log(time);
 //Show the latest time in the <h1> when the Get Time button
 //is pressed.
 
@@ -20,3 +18,5 @@ console.log(time);
 //   console.log("Hey");
 // }
 // setInterval(sayHi, 1000);
+
+ReactDOM.render(<App />, document.getElementById("root"));
